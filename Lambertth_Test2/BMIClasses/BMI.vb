bc978a4aafd8763e856300faa40d1b23ee59ec8a
thlambert <43218422+thlambert@users.noreply.github.com>
2019-11-14 18:42:14 -0600

    Add files via upload

---
+++ Lambertth_Test2/BMIClasses/BMI.vb
@@ -0,0 +1,51 @@
+﻿'------------------------------------------------------------------------------------------------------------
+' Name: Thomas Lambert
+' Date: March 1, 2018
+' Description: Test 2
+'              Base class BMI
+'------------------------------------------------------------------------------------------------------------
+
+
+Public MustInherit Class BMI
+
+   Protected Weight_Min, Weight_Max, Height_Min, Height_Max As Single
+   Protected WeightUnit, HeightUnit As String
+
+   Protected _BMI, _weight, _height As Single
+
+   Public Event Obese()
+
+   Protected MustOverride Sub CalculateBMI()
+
+   Public Property weight As Single
+      Set(value As Single)
+         _weight = value
+         CalculateBMI()
+      End Set
+      Get
+         Return _weight
+      End Get
+   End Property
+
+   Public Property height As Single
+      Set(value As Single)
+         _height = value
+         CalculateBMI()
+      End Set
+      Get
+         Return _height
+      End Get
+   End Property
+   Public Sub New(ByVal h As Single, ByVal W As Single)
+      If (h > Height_Min And h < Height_Max) Then
+         _height = h
+      Else
+         Throw New Exception("IH")
+      End If
+      If (W > Weight_Min And h < Weight_Max) Then
+         _weight = W
+      Else
+         Throw New Exception("IW")
+      End If
+   End Sub
+End Class
---
+++ Lambertth_Test2/BMIClasses/BMISubClasses.vb
@@ -0,0 +1,39 @@
+﻿'------------------------------------------------------------------------------------------------------------
+' Name: Thomas Lambert
+' Date: March 1, 2018
+' Description: Test 2
+'              BMI sub-classes
+'------------------------------------------------------------------------------------------------------------
+
+Public Class BMIInternational
+   Inherits BMI
+
+   Public Sub New(ByVal h As Single, ByVal W As Single)
+      MyBase.New(h, W)
+      WeightUnit = "kilograms"
+      HeightUnit = "meters"
+      _BMI = 20
+      _height = 1
+      _weight = 20
+   End Sub
+   Protected Overrides Sub CalculateBMI()
+
+   End Sub
+End Class
+
+
+Public Class BMIAmerican
+   Inherits BMI
+   Public Sub New(ByVal h As Single, ByVal W As Single)
+      MyBase.New(h, W)
+      WeightUnit = "pounds"
+      HeightUnit = "inches"
+      _BMI = 20
+      _height = 1
+      _weight = 20
+   End Sub
+
+   Protected Overrides Sub CalculateBMI()
+
+   End Sub
+End Class
